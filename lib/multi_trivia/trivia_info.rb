8e1e972f6f046025caeab4d46ebdeb683747d9c3
Abner <abnercabrera28@gmail.com>
2021-02-27 18:58:39 -0500

    removed comments

--- lib/multi_trivia/trivia_info.rb
+++ lib/multi_trivia/trivia_info.rb
@@ -22,20 +22,6 @@ class Trivia::Info
 
     def self.category_list
         return self.question_list.uniq
-        
     end
-
-    # def self.find_cat(cat)
-    #     gory = self.all.find do |object|
-    #       object.category == cat
-    #     end
-    #     puts HTMLEntities.new.decode gory.question
-    # end
-
-    # def self.select_cat
-    #     self.all.select do |object|
-    #         object.category
-    #     end
-    # end
 
 end
